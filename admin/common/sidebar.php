2594f5fa34643df27d95d280663fcfc4ffc63aec
Plamen Stoev <plamen.stoev@rewardgateway.com>
2016-02-06 13:19:43 +0200

    structure changes

---
+++ admin/common/sidebar.php
@@ -0,0 +1,25 @@
+<div class="container-fluid-full">
+    <div class="row-fluid">
+
+        <!-- start: Main Menu -->
+        <div id="sidebar-left" class="span2">
+            <div class="nav-collapse sidebar-nav">
+                <ul class="nav nav-tabs nav-stacked main-menu">
+                    <li><a href="index.html"><i class="icon-bar-chart"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+                    <li><a href="users.php"><i class="icon-envelope"></i><span class="hidden-tablet">Users</span></a></li>
+                    <li><a href="categories.php"><i class="icon-envelope"></i><span class="hidden-tablet"> Categories</span></a></li>
+                    <li><a href="tours.php"><i class="icon-envelope"></i><span class="hidden-tablet"> Tours</span></a></li>
+                    <li><a href="blog.php"><i class="icon-envelope"></i><span class="hidden-tablet"> Blog posts</span></a></li>
+                    <li><a href="categories.php"><i class="icon-envelope"></i><span class="hidden-tablet"> Categories</span></a></li>
+
+                </ul>
+            </div>
+        </div>
+        <!-- end: Main Menu -->
+
+        <noscript>
+            <div class="alert alert-block span10">
+                <h4 class="alert-heading">Warning!</h4>
+                <p>You need to have <a href="http://en.wikipedia.org/wiki/JavaScript" target="_blank">JavaScript</a> enabled to use this site.</p>
+            </div>
+        </noscript>
